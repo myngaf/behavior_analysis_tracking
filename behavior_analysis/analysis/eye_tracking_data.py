2c64791a70468a8e452f0b73ba49e1ad2e519e8d
myngaf <ngmanyung222@gmail.com>
2025-06-29 17:08:19 +0200

    can now be used to calculate eye convergence angles, threshold, score, and spits out a histogram for the distribution of eye angles

--- behavior_analysis/analysis/eye_tracking_data.py
+++ behavior_analysis/analysis/eye_tracking_data.py
@@ -4,6 +4,7 @@ from ..utilities.plotting_helpers import BasePlotting
 from .csv import import_csvs
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 
 from pathlib import Path
@@ -22,6 +23,226 @@ class EyeConvergenceAnalyser(BasePlotting):
         self.threshold = default_threshold
         self.min_threshold = min(threshold_limits)
         self.max_threshold = max(threshold_limits)
+
+    def kernel_density_estimation(self):
+        """Performs kernel density estimation of the data using a gaussian with the given bandwidth.
+
+        Returns
+        -------
+        self
+
+        Notes
+        -----
+        After calling this method, the following attributes become accessible:
+        min_angle, max_angle, bin_edges, counts, kde
+        """
+        self.min_angle = np.floor(self.data.min())
+        self.max_angle = np.ceil(self.data.max())
+        self.bin_edges = np.arange(self.min_angle, self.max_angle + 1)
+        self.counts, self.bin_edges = np.histogram(self.data, bins=self.bin_edges)
+        if self.verbose:
+            print('Performing kernel density estimation...',)
+        # perform kernel density estimation
+        self.kde = KernelDensity(kernel='gaussian', bandwidth=self.bandwidth).fit(np.expand_dims(self.data, 1))
+        # get the log counts
+        log_counts = self.kde.score_samples(np.expand_dims(self.bin_edges, 1))
+        # convert logarithmic values to absolute counts
+        self.kde_counts = np.exp(log_counts)
+        # find the value of the mode
+        self.mode = self.bin_edges[np.argmax(self.kde_counts)]
+        if self.verbose:
+            print('done!')
+        return self
+
+    def find_convergence_threshold(self):
+        """Finds the antimode of kernel density estimated distribution and sets it as the convergence threshold.
+
+        Returns
+        -------
+        threshold : int
+            The threshold used to define eye convergence (the antimode of the distribution of eye vergence angles).
+
+        Notes
+        -----
+        This method finds the local minimum of the eye convergence distribution that lies within the threshold limits.
+        If a local minimum does not exist within the limit, the convergence threshold defaults to the initial value.
+
+        Smoothed distribution:
+           _
+          / \   _
+         /   \_/ \
+        /         \
+
+        First derivative:
+
+        + \     _
+        0  \   / \
+        -   \_/   \
+
+        Local maxima and minima occur when the first derivative is zero. Specifically, local minima occur when there is
+        an inversion of the sign of the first derivative (from negative to positive).
+
+        Sign of first derivative:
+
+        + .    .
+        0  .  . ..
+        -   ..    .
+
+        Second derivative:
+
+         (local min)
+              |
+              V
+        +     _
+        0    / \_
+        -  _/    \
+
+        """
+        # take the derivative of the distribution
+        diffed = np.diff(self.kde_counts)
+        # smooth the differentiated data
+        smoothed = pd.Series(diffed).rolling(7, min_periods=0, center=True).mean().values
+        # take the sign of the smoothed data (is the function increasing or decreasing)
+        signed = np.sign(smoothed)
+        # take the derivative of the sign of the first derivative
+        second_diff = np.diff(signed)
+        # find the indices of local minima (i.e. where the sign of first derivative goes from negative to positive)
+        local_minima = np.where(second_diff > 0)[0] + 1
+        # find values of the antimodes
+        antimodes = self.bin_edges[local_minima]
+        try:
+            # Try to find an antimode within the threshold range
+            self.threshold = antimodes[(antimodes > self.min_threshold) & (antimodes < self.max_threshold)][0]
+        except IndexError:  # local minimum does not exist within the threshold range
+            if self.verbose:
+                print('No local minimum within limits!')
+        if self.verbose:
+            print('Eye convergence threshold:', self.threshold)
+        return self.threshold
+
+    def calculate_convergence_score(self):
+        """Calculates the convergence score, i.e. the proportion of frames above the convergence threshold.
+
+        Returns
+        -------
+        convergence_score: float
+            The proportion of frames that are above the convergence threshold.
+        """
+        # find frames that are above threshold
+        above_threshold = self.data[self.data >= self.threshold]
+        # get number of frames above threshold
+        converged_counts = len(above_threshold)
+        # get total number of frames in the data
+        total_counts = len(self.data)
+        # calculate proportion of frames that are above threshold
+        score = float(converged_counts) / float(total_counts)
+        self.convergence_score = score
+        if self.verbose:
+            print('Eye convergence score:', self.convergence_score)
+        return self.convergence_score
+
+    def plot_histogram(self, save=False, output_path=None):
+        """Plots a histogram of eye vergence angles.
+
+        Parameters
+        ----------
+        save : bool, optional (default = False)
+            - True: the figure is saved to the specified or default output path
+            - False: the figure is shown in a window
+
+        output_path : str, optional (default = None)
+            Where to save the figure if save == True. The default path is: 'histogram_of_eye_convergence_angles.png'
+        """
+        fig, ax = plt.subplots(figsize=(8, 6))
+        fig.suptitle('Distribution of eye vergence angles')
+
+        ax.hist(self.data, bins=self.bin_edges)
+
+        ax.set_xlabel('Angle (degrees)')
+        ax.set_ylabel('Number of frames')
+
+        if save:
+            if output_path is None:
+                output_path = 'histogram_of_eye_convergence_angles.png'
+            self.save_figure(fig, output_path)
+        else:
+            plt.show()
+
+    def plot_kernel_density_estimation(self, save=False, output_path=None):
+        """Plots a histogram of observed eye vergence angles and the estimated distribution.
+
+        Parameters
+        ----------
+        save : bool, optional (default = False)
+            - True: the figure is saved to the specified or default output path
+            - False: the figure is shown in a window
+
+        output_path : str, optional (default = None)
+            Where to save the figure if save == True. The default path is:
+            'kernel_density_estimation_of_eye_convergence_angles.png'
+        """
+        fig, ax = plt.subplots(figsize=(8, 6))
+        fig.suptitle('Kernel density estimation of eye vergence angles')
+
+        ax.hist(self.data, bins=self.bin_edges)
+        ax.plot(self.bin_edges, self.kde_counts * len(self.data), linewidth=3)
+
+        ax.set_xlabel('Angle (degrees)')
+        ax.set_ylabel('Number of frames')
+
+        if save:
+            if output_path is None:
+                output_path = 'kernel_density_estimation_of_eye_convergence_angles.png'
+            self.save_figure(fig, output_path)
+        else:
+            plt.show()
+
+    def plot_threshold(self, save=False, output_path=None):
+        """Plots a histogram of observed eye vergence angles and the estimated distribution.
+
+        Makes two subplots. The left subplot shows the estimated distribution of eye vergence angles with everything
+        above threshold shaded. The right subplot shows the observed distribution of eye vergence angles and the
+        threshold (which is used to calculate the convergence_score).
+
+        Parameters
+        ----------
+        save : bool, optional (default = False)
+            - True: the figure is saved to the specified or default output path
+            - False: the figure is shown in a window
+
+        output_path : str, optional (default = None)
+            Where to save the figure if save == True. The default path is: 'eye_convergence_threshold.png'
+        """
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6), sharex=True, sharey=True)
+        fig.suptitle('Eye convergence threshold')
+
+        converged = self.bin_edges >= self.threshold
+
+        ax1.plot(self.bin_edges, self.kde_counts * len(self.data), linewidth=3)
+        ax1.fill_between(self.bin_edges[converged], 0, self.kde_counts[converged] * len(self.data))
+
+        ax1.set_title('Kernel density estimation')
+        ax1.set_xlabel('Angle (degrees)')
+        ax1.set_ylabel('Counts')
+
+        ax2.hist(self.data, bins=self.bin_edges)
+        ax2.plot([self.threshold, self.threshold], [0, self.counts.max()], c='k', linestyle='dashed')
+
+        ax2.set_title('Raw counts')
+        ax2.set_xlabel('Angle (degrees)')
+
+        ax1.set_xlim(-25, 100)
+        ax1.set_xticks(np.arange(-25, 125, 25))
+
+        if save:
+            if output_path is None:
+                output_path = 'eye_convergence_threshold.png'
+            self.save_figure(fig, output_path)
+            plt.close(fig)
+        else:
+            plt.show()
+
+    plot_kde = plot_kernel_density_estimation
 
     @staticmethod
     def import_eye_angles(paths):
@@ -49,6 +270,8 @@ class EyeConvergenceAnalyser(BasePlotting):
         fish_angles['convergence'] = (fish_angles['right_angle'] - fish_angles['left_angle'])
         print('\ndone!\n')
         return fish_angles
+    
+    
 
 class EyeTrackingData(object):
     """Class for analysing eye angle data from multiple animals.
@@ -142,10 +365,10 @@ class EyeTrackingData(object):
 
         print_heading('CALCULATING CONVERGENCE SCORES')
         IDs, scores, thresholds = [], [], []
-        for ID, eye_angles in self.data.iteritems(): #need to fix this!!!
+        for ID in self.metadata["ID"]: #need to fix this!!!
             print_subheading(ID)
 
-            ECA = EyeConvergenceAnalyser(eye_angles['convergence'], **kwargs)
+            ECA = EyeConvergenceAnalyser(self.data[ID]['convergence'], **kwargs)
             ECA.kernel_density_estimation()
             ECA.find_convergence_threshold()
             ECA.calculate_convergence_score()
